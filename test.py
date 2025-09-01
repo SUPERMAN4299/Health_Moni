8bf151efd094a64d9de21a86ffdeed791267504a
SUPERMAN4299 <135334400+SUPERMAN4299@users.noreply.github.com>
2025-09-01 17:19:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -10,13 +10,13 @@ result = subprocess.run(cmd, capture_output=True, text=True, shell=True)
 output = result.stdout
 print("🔍 Raw output:\n", output)
 
-# Extract MACs
+# Extract MAC
 matches = re.findall(r'DEV_([0-9A-F]{12})', output, re.IGNORECASE)
 mac_list = [m.lower() for m in matches]
 
 print("📡 Found Bluetooth MACs:", mac_list)
 
 if target_mac.lower() in mac_list:
-    print("✅ Device is paired/connected!")
+    print("Device is paired/connected!")
 else:
-    print("❌ Device not found.")
+    print(" Device not found.")
